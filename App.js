9175cb6135a906fba45871de24d20d0dbbbb5aa1
AJ <heyajayramola@gmail.com>
2023-12-17 17:33:08 +0530

    project start

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+// project imports
+import { NativeWindStyleSheet } from "nativewind";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import Trending from './screens/Tending';
 
+// screens navigator
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} 
+      options={{
+        title: "Home",
+        headerShown: true,
+        tabBarActiveTintColor: "gold",
+        tabBarIcon: ({size,focused,color}) => {
+          return (
+            <Image
+              style={{ width: size, height: size }}
+              source={require('./assets/home.png')}
+            />
+          );
+        },
+      }}
+      />
+      <Tab.Screen name="Trending" component={Trending}
+      options={{
+        title: "Trending",
+        headerShown: true,
+        tabBarActiveTintColor: "gold",
+        tabBarIcon: ()=>{
+          return(
+            <Image
+            source={require('./assets/flame.png')}
+            />
+          )
+        }
+      }}
+      />
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+NativeWindStyleSheet.setOutput({
+  default: "native",
 });
---
+++ screens/Tending.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Trending = () => {
+    return (
+        <View>
+            <Text>Most Trending Movies</Text>
+        </View>
+    );
+}
+
+export default Trending;
